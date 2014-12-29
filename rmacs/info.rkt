9813d6dfe0b1cea61d59a62b9d01d1d98a3d9071
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-12-29 18:55:33 -0500

    rmacs launcher

--- rmacs/info.rkt
+++ rmacs/info.rkt
@@ -6,3 +6,6 @@
    `(p "Emacs-like editor.")))
 (define homepage "https://github.com/tonyg/racket-ansi")
 (define primary-file "main.rkt")
+
+(define racket-launcher-names '("rmacs"))
+(define racket-launcher-libraries '("main.rkt"))
