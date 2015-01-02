bf1ca5c6dbbea9c549b316fc0f68fb28228ef3e5
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-02 02:36:04 -0500

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,9 @@
 #lang setup/infotab
 (define collection 'multi)
-(define deps '("base" "ansi"))
+(define deps '("base" "ansi"
+               ;; this for error reporting (only):
+               "web-server-lib"
+               ;; plus these, needed for the tests:
+               "profile-lib"
+               ))
 (define build-deps '("rackunit-lib"))
