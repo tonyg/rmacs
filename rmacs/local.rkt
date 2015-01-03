48786306069415ec85733bf23a026d6de3733626
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-02 21:41:52 -0500

    Make locals defined with an initializer thunk rather than a value

--- rmacs/local.rkt
+++ rmacs/local.rkt
@@ -4,22 +4,32 @@
          make-local
          define-local-definer)
 
+(require racket/match)
+
 ;; A LocalsTable is a (HashEqTable Symbol Any).
 
 (define (make-locals) (hasheq))
 
-(define (make-local get-locals set-locals! name [default #f])
+(define (make-local get-locals set-locals! name [default-thunk (lambda () #f)])
   (case-lambda
     [(thing)
-     (hash-ref (get-locals thing) name (lambda () default))]
+     (hash-ref (get-locals thing) name (lambda ()
+                                         (define val (default-thunk))
+                                         (set-locals! thing (hash-set (get-locals thing)
+                                                                      name
+                                                                      val))
+                                         val))]
     [(thing val)
-     (set-locals! thing (if (equal? val default)
-                            (hash-remove (get-locals thing) name)
-                            (hash-set (get-locals thing) name val)))
+     (match thing
+       ['#:clear (set-locals! val (hash-remove (get-locals val) name))]
+       [_ (set-locals! thing (hash-set (get-locals thing) name val))])
      val]))
 
 (define-syntax-rule (define-local-definer definer get-locals set-locals!)
   (define-syntax definer
     (syntax-rules ()
       ((_ name) (define name (make-local get-locals set-locals! 'name)))
-      ((_ name default) (define name (make-local get-locals set-locals! 'name default))))))
+      ((_ name default) (define name (make-local get-locals
+                                                 set-locals!
+                                                 'name
+                                                 (lambda () default)))))))
