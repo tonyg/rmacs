5c0dc567c9ecc16d6a4176709c39a2844693716b
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-13 22:00:04 -0500

    Simplify gen:buffer-source path access

--- rmacs/file.rkt
+++ rmacs/file.rkt
@@ -1,19 +1,20 @@
 #lang racket/base
 
 (provide gen:buffer-source
+         buffer-source-path
          buffer-source-title-pieces
          buffer-source-mtime
          buffer-source-read
          buffer-source-write
 
-         (struct-out local-file-buffer-source)
-         local-file-buffer-source-path)
+         (struct-out local-file-buffer-source))
 
 (require racket/generic)
 (require (only-in racket/file file->string))
 (require (only-in racket/path normalize-path))
 
 (define-generics buffer-source
+  (buffer-source-path buffer-source)
   (buffer-source-title-pieces buffer-source)
   (buffer-source-mtime buffer-source)
   (buffer-source-read buffer-source)
@@ -22,16 +23,17 @@
 (struct local-file-buffer-source (filename)
         #:transparent
         #:methods gen:buffer-source
-        [(define (buffer-source-title-pieces src)
-           (reverse (map path->string (explode-path (local-file-buffer-source-path src)))))
+        [(define (buffer-source-path src)
+           (normalize-path (simplify-path (local-file-buffer-source-filename src))))
+         (define (buffer-source-title-pieces src)
+           (reverse (map path->string (explode-path (buffer-source-path src)))))
          (define (buffer-source-mtime src)
-           (file-or-directory-modify-seconds (local-file-buffer-source-path src)))
+           (file-or-directory-modify-seconds (buffer-source-path src)))
          (define (buffer-source-read src)
-           (file->string (local-file-buffer-source-path src)))
+           (define p (buffer-source-path src))
+           (if p (file->string p) ""))
          (define (buffer-source-write src content)
-           (call-with-output-file (local-file-buffer-source-path src)
+           (call-with-output-file (buffer-source-path src)
              (lambda (p) (write-string content p))
-             #:exists 'replace))])
-
-(define (local-file-buffer-source-path src)
-  (normalize-path (simplify-path (local-file-buffer-source-filename src))))
+             #:exists 'replace))
+         ])
